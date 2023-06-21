e007afc0e91b88dcba2be33271e95fa72e283849
tadghh <thenry.he@gmail.com>
2023-06-20 20:46:45 -0500

    Moved settings item into one componenet

--- components/settings/settings-item.js
+++ components/settings/settings-item.js
@@ -1,9 +1,38 @@
-export default function SettingsItem( {
+/* eslint-disable camelcase */
+import { useState, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
+
+export default function SettingsItem({
 	settingsTitle,
-	settingsMethod,
-	children,
+	settingsConfigString,
+
 	settingsDescription = "",
-} ) {
+}) {
+	const [settingsItemStatus, setSettingsItemStatus] = useState("");
+
+	const updateOption = () => {
+		console.log("yo we changed" + settingsConfigString);
+		invoke("change_configuration_option", {
+
+			option_name: settingsConfigString,
+			value: settingsItemStatus,
+		}).then(data => {
+			console.log(data);
+		});
+	};
+
+	useEffect(() => {
+		invoke("get_configuration_option", {
+			option_name: settingsConfigString,
+		}).then(data => {
+			if (data == null) {
+				setSettingsItemStatus("Not set");
+			} else {
+				setSettingsItemStatus(data);
+			}
+		});
+	}, []);
 	return (
 		<div className=" w-full border rounded-xl h-14 bg-gray-200 flex items-center justify-between">
 			<div className=" flex  text-gray-900  ">
@@ -12,10 +41,23 @@ export default function SettingsItem( {
 			</div>
 			<form className=" ">
 				<div className="flex gap-x-4 px-2">
-					{children}
+					<span
+						type="text"
+						onClick={() => {
+							open({
+								directory: true,
+								multiple: false,
+							}).then(data => {
+								setSettingsItemStatus(data);
+							});
+						}}
+						className="flex-none rounded-md bg-white px-5 py-2.5 text-sm font-semibold text-blue-500  shadow-sm "
+					>
+						{settingsItemStatus}
+					</span>
 					<button
 						type="button"
-						onClick={settingsMethod}
+						onClick={updateOption}
 						className=" rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 					>
 						Apply
--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,52 +1,18 @@
 /* eslint-disable camelcase */
 import SettingsItem from "@/components/settings/settings-item";
-import {open} from "@tauri-apps/api/dialog";
-import {useEffect, useState} from "react";
-import {invoke} from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
 
 export default function Settings() {
-	const [directoryText, setDirectoryText] = useState("");
-	useEffect(() => {
-		invoke("get_configuration_option", {
-			option_name: "book_folder_location",
-		}).then(data => {
-			if (data == null) {
-				setDirectoryText("Not set");
-			} else {
-				setDirectoryText(data);
-			}
-		});
-	}, []);
+
 	return (
 		<div className="ml-20 flex min-h-screen bg-white  px-5  py-2">
 			<SettingsItem
 				settingsTitle="Book directory"
 				settingsDescription="The folder containing your books"
-				settingsMethod={() => {
-					console.log("yo");
-					invoke("change_configuration_option", {
-						option_name: "book_folder_location",
-						value: directoryText,
-					}).then(data => {
-						console.log(data);
-					});
-				}}
-			>
-				<span
-					type="text"
-					onClick={() => {
-						open({
-							directory: true,
-							multiple: false,
-						}).then(data => {
-							setDirectoryText(data);
-						});
-					}}
-					className="flex-none rounded-md bg-white px-5 py-2.5 text-sm font-semibold text-blue-500  shadow-sm "
-				>
-					{directoryText}
-				</span>
-			</SettingsItem>
+				settingsConfigString="book_folder_location"
+			/>
+
+
 		</div>
 	);
 }
