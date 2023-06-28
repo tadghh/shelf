24992146fcce66429c686ebb7d427d8a8868f212
tadghh <thenry.he@gmail.com>
2023-06-28 18:01:00 -0500

    Settings page cleaning

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -4,18 +4,16 @@ import { SettingsEnum } from "@/lib/SettingsTypeEnum";
 import { SettingsItems } from "@/lib/SettingsItemEnum";
 
 export default function Settings() {
-	//The settings themselves should also be enums
 	return (
 		<div className="flex-col min-h-screen px-5 py-2 ml-20 bg-white">
 			<SettingsItem
-
 				settingsTitle="Book directory"
 				settingsDescription="The folder containing your books"
 				settingsConfigString={SettingsItems.BOOK_LOCATION}
 				settingsType={SettingsEnum.FILE}
 			/>
 			<SettingsItem
-				settingsTitle="Endless Scrolling"
+				settingsTitle="Endless scrolling"
 				settingsDescription="The next page will load as you scroll"
 				settingsConfigString={SettingsItems.ENDLESS_SCROLL}
 				settingsType={SettingsEnum.TOGGLE}
